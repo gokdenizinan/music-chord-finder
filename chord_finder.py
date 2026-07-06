ec456ccacd2acc0442b82c94520ca28ae8c856fc
gokdenizinan <gokdeniz.inan@wur.nl>
2026-07-06 22:59:51 +0200

    Note values, normalize note and parse user input has been implemented

--- chord_finder.py
+++ chord_finder.py
@@ -1,1 +1,95 @@
-print("Music chord finder is running")
+"""
+Music Chord Finder - v1
+
+Goal:
+Identify basic chords from note names.
+
+Supported in v1:
+- Major chords
+- Minor chords
+- Diminished chords
+- Augmented chords
+- Inversions
+- Sharp notes only
+"""
+
+
+NOTE_VALUES = {
+    "C": 0,
+    "C#": 1,
+    "D": 2,
+    "D#": 3,
+    "E": 4,
+    "F": 5,
+    "F#": 6,
+    "G": 7,
+    "G#": 8,
+    "A": 9,
+    "A#": 10,
+    "B": 11,
+
+}
+
+
+CHORD_PATTERNS = {
+    # "major": ...
+    # "minor": ...
+    # "diminished": ...
+    # "augmented": ...
+}
+
+
+def normalize_note(note): # one note at a time
+    if len(note) == 1:
+        note = note.upper()
+    else:
+        note = note[0].upper() + note[1]
+    return note
+
+def parse_user_input(user_input): #C E G#
+    return user_input.split()
+
+
+
+
+def notes_to_numbers(notes):
+    pass
+
+
+def get_intervals(note_numbers, root_number):
+    pass
+
+
+def match_chord(intervals):
+    pass
+
+
+def find_possible_chords(notes):
+    pass
+
+
+def format_chord_name(root_note, chord_type):
+    pass
+
+
+def print_results(results):
+    pass
+
+
+def main():
+    print("Welcome to Chord Finder!")
+    user_input = input("Enter notes: ")
+
+    print(parse_user_input(user_input))
+
+    sep_input = parse_user_input(user_input)
+
+    i = 0
+    for note in sep_input:
+        sep_input[i] = normalize_note(note)
+        i+=1
+    print(sep_input)
+
+
+if __name__ == "__main__":
+    main()
